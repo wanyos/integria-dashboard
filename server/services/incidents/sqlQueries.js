f64b4a499759ee03561e94e5a937cf69fe988639
wanyos99 <juanjor99@gmail.com>
2024-12-01 13:27:19 +0100

    create sql to donnut places, localizations

--- server/services/incidents/sqlQueries.js
+++ server/services/incidents/sqlQueries.js
@@ -48,6 +48,30 @@ const QUERIES = {
   allIncCloseHorizontales: `SELECT COUNT(*) as count FROM tincidencia WHERE inicio >= ? AND inicio < DATE_ADD(?, INTERVAL 1 DAY) AND cierre > '0001-01-01' AND id_grupo IN (19,84,86,87,122,126,149,141,21);`,
   allIncCloseNegocio: `SELECT COUNT(*) as count FROM tincidencia WHERE inicio >= ? AND inicio < DATE_ADD(?, INTERVAL 1 DAY) AND cierre > '0001-01-01' AND id_grupo IN (85,20,23,40,90,91,101,43,147);`,
   allIncCloseExterno: `SELECT COUNT(*) as count FROM tincidencia WHERE inicio >= ? AND inicio < DATE_ADD(?, INTERVAL 1 DAY) AND cierre > '0001-01-01' AND id_grupo IN (22,24,28,31,56,154,50,52,59,32);`,
+
+  // Total de incidencias por rango de fechas y su localización
+  allIncLocationRange: `  SELECT count(*) as total FROM tincident_field_data R
+LEFT JOIN
+    tincidencia I
+    ON R.id_incident = I.id_incidencia
+    AND R.id_incident_field IN (98, 103, 109, 114)
+LEFT JOIN
+    tincident_status S
+    ON I.estado = S.id
+LEFT JOIN
+    tusuario U
+    ON I.id_creator = U.id_usuario
+LEFT JOIN
+    tusuario E
+    ON I.id_usuario = E.id_usuario
+LEFT JOIN
+    tgrupo G
+    ON I.id_grupo = G.id_grupo
+LEFT JOIN
+    tincident_type T
+    ON I.id_incident_type = T.id
+WHERE inicio >= ? AND inicio < DATE_ADD(?, INTERVAL 1 DAY)
+       AND R.data IN (?);`
 }
 
 export default QUERIES
